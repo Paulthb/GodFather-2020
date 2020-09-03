9706c8b424115869fc624047d61d389aae936109
Matt <matthieu.manuellan@gmail.com>
2020-09-03 21:48:11 +0200

    ntm pol

---
+++ Assets/Scripts/WaterCan/WateringCan.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Rewired;
+
+public class WateringCan : MonoBehaviour
+{
+    public float speed = 5.0f;
+    private Player player;
+    private void Start()
+    {
+        player = ReInput.players.GetPlayer(0);
+    }
+
+    private void Update()
+    {
+        float x = player.GetAxis("X_Axis") * speed * Time.deltaTime;
+        float y = player.GetAxis("Y_Axis") * speed * Time.deltaTime;
+        transform.position = new Vector3(transform.position.x + x, transform.position.y + y, 0);
+    }
+}
+
---
+++ Assets/watergame.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class watergame : MonoBehaviour
+{
+    public int waterAmount = 0;
+    public int waterNeeded = 0;
+    public float mainTimer = 20.0f;
+
+    public Image timerFillImage;
+    public SpriteRenderer Plant;
+    public List<Sprite> plantStates;
+    private int state = 0;
+
+    private bool hasEnded = false;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!hasEnded)
+        {
+            mainTimer -= Time.deltaTime;
+            if (mainTimer <= 0)
+            {
+                //Défaite
+                hasEnded = true;
+                SoundManager.Instance.StartLoose();
+            }
+            else
+            {
+                switch (state)
+                {
+                    case 0:
+                        if(waterAmount >= waterNeeded/3)
+                        {
+                            Plant.sprite = plantStates[0];
+                            state = 1;
+                        }
+                        break;
+                    case 1:
+                        if (waterAmount >= waterNeeded / 2)
+                        {
+                            Plant.sprite = plantStates[1];
+                            state = 2;
+                        }
+                        break;
+                    case 2:
+                        if(waterAmount >= waterNeeded)
+                        {
+                            Plant.sprite = plantStates[2];
+                        }
+                        break;
+                }
+                if(waterAmount >= waterNeeded)
+                {
+                    //Victoire
+                    hasEnded = true;
+                    GameManager.Instance.AddScore((int)(mainTimer * 100));
+                    SoundManager.Instance.StartLoose();
+                    GameManager.Instance.LaunchTransition();
+                }
+            }
+        }
+        
+    }
+}
