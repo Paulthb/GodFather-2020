5c6db78d0fe32cd39abff721262d7111689c523b
Paul Thibaul <Paulthb1@gmail.com>
2020-09-04 09:40:33 +0200

    fix highscore

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -89,7 +89,7 @@ public class GameManager : MonoBehaviour
             nextLevelText.text = NextLevelChoice();
 
 
-        yield return new WaitForSeconds(2f);
+        yield return new WaitForSeconds(1.5f);
         LaunchNextGame();
         nextLevelText.text = "";
 
